35000378e13946112268da95eb7fccb689d4c356
Ainur Dzholdosheva <ajnurdzoldoseva@gmail.com>
2020-03-28 23:56:17 +0600

    Added TableServing component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
+import TableServing from './containers/TableServing/TableServing';
 
 export default () => {
   return (
     <div className="App">
       <Layout>
-       Table serving
+       <TableServing/>
       </Layout>
     </div>
   );
---
+++ src/containers/TableServing/TableServing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import classes from './TableServing.module.css';
+
+export default () => (
+    <div className={classes.TableServing}>
+      TableKit
+      TableControls  
+    </div>
+);
