8b2c06ffd52475873d33b67a6b47edf9f2580814
Ainur Dzholdosheva <ajnurdzoldoseva@gmail.com>
2020-04-13 09:21:46 +0600

    img

--- src/components/Layout/Logo/Logo.js
+++ src/components/Layout/Logo/Logo.js
@@ -4,7 +4,7 @@ import classes from './Logo.module.css';
 
 export default () => (
  <div className={classes.Logo}>
-     <img src={logo.svg} width="50" height="50"/>
+     <img src="https://vectr.com/ainurdzholdosheva/b7CTKHd2F.svg?width=250&height=250&select=b7CTKHd2Fpage0" />
      <span>Table serving</span>
  </div>  
 );
