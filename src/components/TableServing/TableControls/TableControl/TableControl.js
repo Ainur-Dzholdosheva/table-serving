456e62a08edd31fa75e8dcbf6c693630701c910c
Ainur Dzholdosheva <ajnurdzoldoseva@gmail.com>
2020-04-13 09:55:58 +0600

    Table control changes

--- src/components/TableServing/TableControls/TableControl/TableControl.js
+++ src/components/TableServing/TableControls/TableControl/TableControl.js
@@ -4,10 +4,13 @@ import classes from './TableControl.module.css'
 export default({control, removeIngredient, addIngredient, disabled}) =>{
     return(
         <div className={classes.TableControl}>
-          <span className={classes.label}>{control.label}</span> 
-              <button className={classes.less} 
+
+<button className={classes.less} 
               onClick={() => removeIngredient(control.type)} 
               disabled={disabled}>less</button>
+              
+          <span className={classes.label}>{control.label}</span> 
+             
               <button className={classes.more} onClick={() => addIngredient(control.type)}>more</button>
         </div>
     )
