3e31b0f90c69d258fc59ca268aa0abc3ecdd256f
Ainur Dzholdosheva <ajnurdzoldoseva@gmail.com>
2020-04-18 00:11:40 +0600

    toolbar

--- src/components/Layout/Toolbar/Navigation/NavigationItem/NavigationItem.js
+++ src/components/Layout/Toolbar/Navigation/NavigationItem/NavigationItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import classes from './NavigationItem.module.css';
 
-export default({children, url}) => {
+export default({children, url, active}) => {
     return(
         <div className={classes.NavigationItem}>
-         <li><a href={url}>{children}</a></li>
+         <li>
+             <a href={url} className={active ? classes.active: null}>{children}</a></li>
         </div>
     )
 }
