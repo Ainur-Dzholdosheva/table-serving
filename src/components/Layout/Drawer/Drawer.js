32f7fe0e73b56f824bcac02f13791cbc990f431f
Ainur Dzholdosheva <ajnurdzoldoseva@gmail.com>
2020-04-18 23:41:13 +0600

    Navigation and Logo were imported to UI

--- src/components/Layout/Drawer/Drawer.js
+++ src/components/Layout/Drawer/Drawer.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import classes from './Drawer.module.css';
+import Logo from '../../UI/Logo/Logo';
+import Navigation from '../../UI/Navigation/Navigation';
 
 export default () => (
-    <div className={classes.Drawer}></div>
+    <div className={classes.Drawer}>
+        <Logo/>
+        <Navigation/>
+    </div>
 );
