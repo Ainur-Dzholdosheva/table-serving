ba84cb219b6c682f8b13d43ce2375b4e85106224
Ainur Dzholdosheva <ajnurdzoldoseva@gmail.com>
2020-04-25 01:51:51 +0600

    delete overflow in Tablekit modules

--- src/components/TableServing/TableKit/TableKit.js
+++ src/components/TableServing/TableKit/TableKit.js
@@ -15,14 +15,17 @@ export default ({price,ingredients}) => {
         "fourth",
       ], 
       index: 0
+
+      
     }
+    
     ingredientsOutput.push(
       <div className={output.classNames[output.index]}> 
         <SeaFood key={type+i} type={type} />
       </div>
     );
 
-    if (output.index > 3) {
+    if (output.index >3) {
       output.index = 0
     }
    }
@@ -31,6 +34,6 @@ export default ({price,ingredients}) => {
   return(
     <div className={classes.TableKit}>
       {ingredientsOutput}
-      <div className={classes.price}> {price} som</div>
+      <span className={classes.price}> {price} som</span>
      </div>
   )};
