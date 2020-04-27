63fb28598dfefede8283c06c440d81589baa214f
Ainur Dzholdosheva <ajnurdzoldoseva@gmail.com>
2020-04-27 22:15:23 +0600

    table-kit background image

--- src/components/TableServing/TableKit/TableKit.js
+++ src/components/TableServing/TableKit/TableKit.js
@@ -1,39 +1,33 @@
-import React from 'react';
-import classes from './TableKit.module.css';
-import SeaFood from './SeaFood/SeaFood';
+import React from "react";
+import classes from "./TableKit.module.css";
+import SeaFood from "./SeaFood/SeaFood";
 
-export default ({price,ingredients}) => {
- let ingredientsOutput=[];
+export default ({ price, ingredients }) => {
+  let ingredientsOutput = [];
 
- Object.keys(ingredients).forEach(type => {
-   for(let i=0; i < ingredients[type]; i++) {
-    let output = {
-      classNames: [
-        "first",
-        "second",
-        "third",
-        "fourth",
-      ], 
-      index: 0
+  Object.keys(ingredients).forEach((type) => {
+    for (let i = 0; i < ingredients[type]; i++) {
+      let output = {
+        classNames: ["first", "second", "third", "fourth"],
+        index: 0,
+      };
 
-      
+      ingredientsOutput.push(
+        <div className={output.classNames[output.index]}>
+          <SeaFood key={type + i} type={type} />
+        </div>
+      );
+
+      if (output.index > 3) {
+        output.index = 0;
+      }
     }
-    
-    ingredientsOutput.push(
-      <div className={output.classNames[output.index]}> 
-        <SeaFood key={type+i} type={type} />
-      </div>
-    );
-
-    if (output.index >3) {
-      output.index = 0
-    }
-   }
-   
- });
-  return(
+  });
+  return (
     <div className={classes.TableKit}>
       {ingredientsOutput}
+
       <span className={classes.price}> {price} som</span>
-     </div>
-  )};
+    </div>
+  );
+};
