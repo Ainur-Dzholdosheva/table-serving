998ef5e9edfa7a847b9dbd1d3c664bcd92c37f25
Ainur Dzholdosheva <ajnurdzoldoseva@gmail.com>
2020-04-28 23:40:20 +0600

    change images in Seafood

--- src/components/TableServing/TableControls/TableControls.js
+++ src/components/TableServing/TableControls/TableControls.js
@@ -1,37 +1,38 @@
-import React from 'react';
-import classes from './TableControls.module.css';
-import TableControl from './TableControl/TableControl.js'
+import React from "react";
+import classes from "./TableControls.module.css";
+import TableControl from "./TableControl/TableControl.js";
 import Button from "../../UI/Button/Button.js";
 
 const CONTROLS = [
-    {label:"Fish", type:"fish"},
-    {label:"Crab", type:"crab"},
-    {label:"Salmon", type:"salmon"},
-    {label:"Lobster", type:"lobster"},
-    {label:"Caviar", type:"cavior"},
-    {label:"Shrimp", type:"shrimp"},
+  { label: "Oysters", type: "fish" },
+  { label: "Crab", type: "crab" },
+  { label: "Salmon", type: "salmon" },
+  { label: "Lobster", type: "lobster" },
+  { label: "Caviar", type: "cavior" },
+  { label: "Shrimp", type: "shrimp" },
 ];
-export default({
-  startOrder, 
+export default ({
+  startOrder,
   canOrder,
   ingredients,
-  addIngredient, 
-  removeIngredient}) => {
-    
-    
-    const controlsOutput = CONTROLS.map(control => 
-    <TableControl 
-    key={control.type}
-    control={control} 
-    addIngredient={addIngredient} 
-    removeIngredient={removeIngredient}
-    disabled={ingredients[control.type]===0} />)
-    return(
+  addIngredient,
+  removeIngredient,
+}) => {
+  const controlsOutput = CONTROLS.map((control) => (
+    <TableControl
+      key={control.type}
+      control={control}
+      addIngredient={addIngredient}
+      removeIngredient={removeIngredient}
+      disabled={ingredients[control.type] === 0}
+    />
+  ));
+  return (
     <div className={classes.TableControls}>
-         {controlsOutput}
-         <Button click={startOrder} enabled={canOrder}>
+      {controlsOutput}
+      <Button click={startOrder} enabled={canOrder}>
         Order
       </Button>
     </div>
-    )
-}
+  );
+};
