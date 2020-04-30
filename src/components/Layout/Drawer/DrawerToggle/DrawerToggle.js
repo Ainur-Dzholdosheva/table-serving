7e116a40c7ac869044f3fb792490def1e7a30256
Ainur Dzholdosheva <ajnurdzoldoseva@gmail.com>
2020-04-30 23:00:02 +0600

    deleted word menu

--- src/components/Layout/Drawer/DrawerToggle/DrawerToggle.js
+++ src/components/Layout/Drawer/DrawerToggle/DrawerToggle.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import classes from './DrawerToggle.module.css';
+import React from "react";
+import classes from "./DrawerToggle.module.css";
 
 export default ({ toggleDrawer }) => (
   <div className={classes.DrawerToggle} onClick={toggleDrawer}>
-      Menu
     <div></div>
     <div></div>
     <div></div>
   </div>
-  ); 
+);
