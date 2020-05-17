b5d6bf04a8f662db2db9825f34a4064ba9cab29f
Ainur Dzholdosheva <ajnurdzoldoseva@gmail.com>
2020-05-17 18:38:02 +0600

    created Order component

--- src/components/Checkout/CheckoutSummary/CheckoutSummary.js
+++ src/components/Checkout/CheckoutSummary/CheckoutSummary.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Table from "../../TableServing/Table/Table";
+import { Route } from "react-router-dom";
 import Button from "../../UI/Button/Button";
 import classes from "./CheckoutSummary.module.css";
 
@@ -7,12 +8,14 @@ export default ({ price, ingredients, checkoutCancel, checkoutContinue }) => {
   return (
     <div className={classes.CheckoutSummary}>
       <Table price={price} ingredients={ingredients} />
-      <Button click={checkoutCancel} red>
-        Cancel
-      </Button>
-      <Button click={checkoutContinue} green>
-        Continue
-      </Button>
+      <Route path="/checkout" exact>
+        <Button click={checkoutCancel} red>
+          Cancel
+        </Button>
+        <Button click={checkoutContinue} green>
+          Continue
+        </Button>
+      </Route>
     </div>
   );
 };
---
+++ src/components/Orders/Order/Order.js
@@ -0,0 +1,6 @@
+import React from "react";
+import classes from "./Order.module.css";
+
+export default () => {
+  return <div className={classes.Order}></div>;
+};
