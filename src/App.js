0dc970ef177a0594c17f7fef5e2be78fa111ed62
Ainur Dzholdosheva <ajnurdzoldoseva@gmail.com>
2020-05-31 18:25:20 +0600

    Restructure containers into components

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
 import Layout from "./containers/Layout/Layout";
 import TableServing from "./containers/TableServing/TableServing";
-import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./containers/Orders/Orders";
+import Checkout from "./components/Checkout/Checkout";
+import Orders from "./components/Orders/Orders";
 
 export default () => {
   return (
